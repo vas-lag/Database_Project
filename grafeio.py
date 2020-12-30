7ae7dcef383b944d0aba7afcb894ef1c72375bc1
ThomasPappas00 <75483971+ThomasPappas00@users.noreply.github.com>
2020-12-30 19:43:54 +0200

    Update grafeio.py

--- grafeio.py
+++ grafeio.py
@@ -179,6 +179,80 @@ c.execute('''CREATE TABLE IF NOT EXISTS THLEFWNO_ERGODOTH (
           ''')  
 
 
+### insert people into tables
+c.execute('DELETE FROM ENDIAFEROMENOS;')
+c.execute('DELETE FROM ERGODOTHS;')
+
+male_names = open('male-first-names-list.txt',encoding="utf8").read().splitlines()
+female_names = open('female-first-names-list.txt',encoding="utf8").read().splitlines()
+male_last_names = open('male-last-names-list.txt',encoding="utf8").read().splitlines()
+female_last_names = open('female-last-names-list.txt',encoding="utf8").read().splitlines()
+emails = open('email-list.txt',encoding="utf8").read().splitlines()
+nomoi = open('nomoi-list.txt',encoding="utf8").read().splitlines()
+addresses = open('addresses-list.txt',encoding="utf8").read().splitlines()
+
+
+for i in range(35):
+    myname = random.choice(male_names)
+    mylastname = random.choice(male_last_names)
+    age = random.randint(18,65)
+    myemail = random.choice(emails)
+    mynomos = random.choice(nomoi)
+    myaddress = random.choice(addresses)
+
+    male_names.remove(myname)
+    male_last_names.remove(mylastname)
+    emails.remove(myemail)
+    addresses.remove(myaddress)
+
+    c.execute('''INSERT INTO ENDIAFEROMENOS
+    VALUES (?,?,?,?,?,?,?,?)''',(i+1,myname,mylastname,age,'M',myemail,mynomos,myaddress))
+
+for i in range(35):
+    myname = random.choice(female_names)
+    mylastname = random.choice(female_last_names)
+    age = random.randint(18,65)
+    myemail = random.choice(emails)
+    mynomos = random.choice(nomoi)
+    myaddress = random.choice(addresses)
+
+    female_names.remove(myname)
+    female_last_names.remove(mylastname)
+    emails.remove(myemail)
+    addresses.remove(myaddress)
+
+    c.execute('''INSERT INTO ENDIAFEROMENOS
+    VALUES (?,?,?,?,?,?,?,?)''',(i+36,myname,mylastname,age,'F',myemail,mynomos,myaddress))
+
+
+for i in range(15):
+    myname = random.choice(male_names)
+    mylastname = random.choice(male_last_names)
+    myemail = random.choice(emails)
+    myaddress = random.choice(addresses)
+
+    male_names.remove(myname)
+    male_last_names.remove(mylastname)
+    emails.remove(myemail)
+    addresses.remove(myaddress)
+    
+    c.execute('''INSERT INTO ERGODOTHS
+    VALUES (?,?,?,?,?,?)''',(i+1,myname,mylastname,'M',myemail,myaddress))
+
+for i in range(15):
+    myname = random.choice(female_names)
+    mylastname = random.choice(female_last_names)
+    myemail = random.choice(emails)
+    myaddress = random.choice(addresses)
+
+    female_names.remove(myname)
+    female_last_names.remove(mylastname)
+    emails.remove(myemail)
+    addresses.remove(myaddress)
+    
+    c.execute('''INSERT INTO ERGODOTHS
+    VALUES (?,?,?,?,?,?)''',(i+16,myname,mylastname,'F',myemail,myaddress))
+
 
 conn.commit()
 conn.close()
